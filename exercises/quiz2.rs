16cace2f1ddfcf2b26bc4c04c156936b9e045acd
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2024-01-22 19:00:51 +0100

    feat: macros

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -56,7 +56,7 @@ mod my_module {
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     use super::Command;
-    use my_module::transformer;
+    use super::my_module::transformer;
 
     #[test]
     fn it_works() {
